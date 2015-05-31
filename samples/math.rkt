4e61b2463ce93020f2068a5c6020a665db53a532
Artyom Vybornov <art-vybor@ya.ru>
2015-05-31 16:05:40 +0300

    add lambda func to math.lc

--- samples/math.rkt
+++ samples/math.rkt
@@ -3,7 +3,7 @@
 (define (gcd a b) (if (eq? b 0) a (gcd b (remainder a b))))
 (define (abs a) (if (>= a 0) a (- a)))
 (define (sqr x) (binpow x 2))
-(define (binpow a n) (if (eq? n 0) 1 (if (eq? (remainder n 2) 1) (* (binpow a (- n 1)) a) (expt (binpow a (/ n 2)) 2))))
+(define binpow (lambda (a n) (if (eq? n 0) 1 (if (eq? (remainder n 2) 1) (* (binpow a (- n 1)) a) (expt (binpow a (/ n 2)) 2)))))
 (define (fib n) (/ (- (expt (/ (+ 1 (sqrt 5)) 2) n) (expt (/ (- 1 (sqrt 5)) 2) n)) (sqrt 5)))
 (define (fact n) (if (eq? n 0) 1 (* n (fact (- n 1)))))
 (define (print x) (display x) (newline))
