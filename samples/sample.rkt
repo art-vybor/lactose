a923d3c2a987809336cd6848ee131aa1b78cae88
Artyom Vybornov <art-vybor@ya.ru>
2015-06-11 01:03:36 +0300

    report fixed

--- samples/sample.rkt
+++ samples/sample.rkt
@@ -1,4 +1,4 @@
 #lang r5rs
-(define (fact n) (define (loop i) (if (< i n) (* i (loop (+ i 1))) n)) (loop 1))
-(define (main) (display (fact 5)))
+(define (abs a) (if (>= a 0) a (- a)))
+(define (main) (display (- abs 2)))
 (main)
